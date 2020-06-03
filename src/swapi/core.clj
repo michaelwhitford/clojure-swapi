fc1fcc34bce7f1bc475a06fcdb22d21e0a00034d
Michael Whitford <michael@whitford.us>
2020-06-02 20:28:23 -0700

    reducing duplication and adding macro to create action queries

--- src/swapi/core.clj
+++ src/swapi/core.clj
@@ -7,62 +7,56 @@
             [manifold.executor :refer [fixed-thread-executor]]))
 
 (defn reset []
+  (shutdown-agents)
   (require 'swapi.core :reload-all))
 
-(def url (atom "https://swapi.dev/api"))
+(def config (atom {:url "https://swapi.dev/api"
+                   :actions #{"people" "planets" "films"
+                              "starships" "vehicles" "species"}))
 
 (def options (atom {:as :json
                     ; :debug true
                     :accept :json}))
-
 (defn query!
   "query to swapi.dev
-    `action` required: people planets films starships vehicles species
+    `action` required: config set
     `id` optional"
   [action & args]
-  (if-let [id (first args)]
-    (:body (http/get (format "%s/%s/%s" @url action id) @options))
-    (:results (:body (http/get (format "%s/%s" @url action) @options)))))
+  (if (contains? (:actions @config) action)
+    (if-let [id (first args)]
+      (:body (http/get (format "%s/%s/%s" (:url @config) action id) @options))
+      (:results (:body (http/get (format "%s/%s" (:url @config) action) @options))))
+    nil))
 
 (defn query+
   "asynchronous query to swapi.dev
-    `action` required: people planets films starships vehicles species
+    `action` required: config set
     `id` optional"
   [pool action & args]
-  (let [deferred (d/deferred pool)]
-    (if-let [id (first args)]
-      (let [chain (d/chain deferred #(future (query! action %)))]
-        (d/success! deferred id)
-        chain)
-      (let [chain (d/chain deferred #(future (query! %)))]
-        (d/success! deferred action)
-        chain))))
+  (if (contains? (:actions @config) action)
+    (let [id (first args)
+          deferred (d/deferred pool)
+          chain (d/chain deferred #(future (if id (query! action %) (query! %))))]
+      (d/success! deferred (or id action))
+      chain)
+    nil)
+
+(defmacro create-queries!
+  "Create 3 new functions for one action of the api
+  `action` required: config set"
+  [action]
+  (def (str 'action "!") (partial query! 'action))
+  (def (str 'action "+") (partial query+ pool 'action))
+  (def (str 'action "-schema!") (partial (str 'action "!") 'action)))
 
 (def pool (fixed-thread-executor 4))
 
-(def people! (partial query! "people"))
-(def people+ (partial query+ pool "people"))
-(def people-schema! (partial people! "schema"))
-
-(def planets! (partial query! "planets"))
-(def planets+ (partial query+ pool "planets"))
-(def planets-schema! (partial planets! "schema"))
-
-(def films! (partial query! "films"))
-(def films+ (partial query+ pool "films"))
-(def films-schema! (partial films! "schema"))
-
-(def starships! (partial query! "starships"))
-(def starships+ (partial query+ pool "starships"))
-(def starships-schema! (partial starships! "schema"))
-
-(def vehicles! (partial query! "vehicles"))
-(def vehicles+ (partial query+ pool "vehicles"))
-(def vehicles-schema! (partial vehicles! "schema"))
-
-(def species! (partial query! "species"))
-(def species+ (partial query+ pool "species"))
-(def species-schema (partial species! "schema"))
+(create-queries! "people")
+(create-queries! "planets")
+(create-queries! "films")
+(create-queries! "starships")
+(create-queries! "vehicles")
+(create-queries! "species")
 
 (comment
   (people! 1)
